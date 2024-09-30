2acbdcc944f587973f0af9d75cb31fcbc9083e37
Eduardo Ferras <edferras@gmail.com>
2024-09-30 16:45:32 -0300

    add sprite svg

---
+++ src/components/UI/Icon/index.tsx
@@ -0,0 +1,9 @@
+import { SVGProps } from 'react'
+
+export default function Icon({ name, ...props }: SVGProps<SVGSVGElement> & { name: string }) {
+	return (
+		<svg {...props}>
+			<use href={`images/svg/sprite.svg#${name}`} />
+		</svg>
+	)
+}
