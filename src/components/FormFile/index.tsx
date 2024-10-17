ba599002eb70557dc956080f276fd95bda4d8e7e
Eduardo Ferras <edferras@gmail.com>
2024-10-17 01:04:35 -0300

    fix upload frontend

--- src/components/FormFile/index.tsx
+++ src/components/FormFile/index.tsx
@@ -1,17 +1,46 @@
 'use client'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import S from './style.module.scss'
-import UploadFile from '../UploadFile'
+import Icon from '../UI/Icon'
+import { nanoid } from 'nanoid'
+
+interface fileProps {
+	id: string
+	name: string
+	file: File
+}
 
 export default function FormFile() {
-	const [files, setFiles] = useState<FileList>()
+	const [filesSelected, setFilesSelected] = useState<fileProps[]>([])
+
+	function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+		const fileList = e.target.files
+
+		if (!fileList) return
+
+		const arrayFiles: fileProps[] = Array.from(fileList).map((file) => ({
+			file,
+			name: file.name,
+			id: nanoid(),
+		}))
+
+		setFilesSelected(filesSelected.concat(arrayFiles))
+	}
+
+	function handleRemoveFile(id: fileProps['id']): void {
+		if (!filesSelected) return
+
+		const newFiles = filesSelected.filter((item) => item.id !== id)
+
+		setFilesSelected(newFiles)
+	}
 
 	async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
 		e.preventDefault()
 		const formMethod = e.currentTarget.method
 		const formURL = e.currentTarget.action
 
-		if (!files) return
+		// if (!files) return
 
 		try {
 			console.log('foi')
@@ -29,24 +58,45 @@ export default function FormFile() {
 			console.error(error)
 		}
 	}
+
 	return (
 		<div className={S.section}>
-			{files && <UploadFile files={files} />}
+			{filesSelected && (
+				<ul className={S.Upload}>
+					{filesSelected.map((file: fileProps) => (
+						<li key={file.id} className={S.File}>
+							<Icon name="excel" width="32" height="32" />
+							<span className={S.name}>{file.name}</span>
+							<Icon className={S.close} name="close" width="24" height="24" fill="#47474F" onClick={() => handleRemoveFile(file.id)} />
+						</li>
+					))}
+				</ul>
+			)}
 			<form onSubmit={handleSubmit} className={S.form} method="POST" action="/api/uploads">
-				<label htmlFor="uploadFile" className={S.label}>
-					{files ? 'Consultar Nomes' : 'Selecionar arquivo EXCEL'}
-					<input
-						type="file"
-						id="uploadFile"
-						multiple
-						tabIndex={1}
-						autoFocus
-						onChange={(e) => setFiles(e.target.files as FileList)}
-						accept=".xlsx, .xls"
-						aria-required="true"
-						required
-					/>
-				</label>
+				{filesSelected.length > 0 ? (
+					<button type="submit" className={S.submit}>
+						Consultar
+					</button>
+				) : (
+					<label htmlFor="uploadFile" className={S.label}>
+						Selecionar arquivo EXCEL
+						<input
+							type="file"
+							id="uploadFile"
+							multiple
+							tabIndex={1}
+							autoFocus
+							onChange={handleFileChange}
+							accept="
+						.xlsx,
+						.xls,
+						application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+						application/vnd.ms-excel"
+							aria-required="true"
+							required
+						/>
+					</label>
+				)}
 			</form>
 		</div>
 	)
