fe261dae2cf9d3f4216e5402a3600040f9738a8f
Eduardo Ferras <edferras@gmail.com>
2024-12-14 16:16:38 -0300

    refactor(UploadList): separate in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 'use client'
 import S from './page.module.scss'
-import { ChangeEvent, useState } from 'react'
-import Icon from '@/components/UI/Icon'
+import { ChangeEvent, useRef, useState } from 'react'
 import { nanoid } from 'nanoid'
 import { useRouter } from 'next/navigation'
 import { FileItem, FileItems } from '@/types'
 import Loading from '@/components/UI/Loading'
+import UploadList from '@/components/UploadList'
 
 export default function Home() {
 	const [filesSelected, setFilesSelected] = useState<FileItems>([])
 	const [isLoading, setIsLoading] = useState(false)
+	const [message, setMessage] = useState('')
 	const router = useRouter()
+	const hiddenInputFile = useRef<HTMLInputElement>(null)
 
 	function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
 		const fileList = e.target.files
@@ -23,18 +25,13 @@ export default function Home() {
 			id: nanoid(),
 		}))
 
+		setMessage('Arquivo adicionado')
 		setFilesSelected(filesSelected.concat(arrayFiles))
 	}
 
-	function handleRemoveFile(id: FileItem['id']): void {
-		if (!filesSelected) return
-
-		const newFiles = filesSelected.filter((item) => item.id !== id)
-
-		setFilesSelected(newFiles)
-	}
-
-	async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+	async function handleSubmit(
+		e: React.FormEvent<HTMLFormElement>,
+	): Promise<void> {
 		setIsLoading(true)
 		e.preventDefault()
 
@@ -76,51 +73,52 @@ export default function Home() {
 				<Loading />
 			) : (
 				<section className={S.Upload}>
-					<h1 className={S.Upload__title}>Verificar disponibilidade domínio no Registro.br</h1>
-					<p className={S.Upload__description}>Importe uma lista de nomes em Excel para verificar se já possuem domínios registrados.</p>
+					<h1 className={S.Upload__title}>
+						Verificar disponibilidade domínio no Registro.br
+					</h1>
+					<p className={S.Upload__description}>
+						Importe uma lista de nomes em Excel para verificar se já possuem
+						domínios registrados.
+					</p>
 					{filesSelected.length > 0 && (
-						<ul className={S.Upload__list} aria-live={'polite'}>
-							{filesSelected.map((file: FileItem) => (
-								<li key={file.id} className={S.Upload__list__item}>
-									<Icon ariahidden alt="Ícone excel" name="excel" width="32" height="32" />
-									<span className={S.Upload__list__item__name}>{file.name}</span>
-									<button className={S.Upload__list__item__btn} aria-label="Remover arquivo">
-										<Icon
-											className={S.Upload__list__item__btn__remove}
-											alt="Ícone remover item"
-											name="remove"
-											onClick={() => handleRemoveFile(file.id)}
-											ariahidden
-										/>
-									</button>
-								</li>
-							))}
-						</ul>
+						<UploadList files={filesSelected} setFiles={setFilesSelected} />
 					)}
-					<form onSubmit={handleSubmit} className={S.Upload__form} method="POST" action="/api/upload" encType="multiplart/form-data">
+					<form
+						onSubmit={handleSubmit}
+						className={S.Upload__form}
+						method="POST"
+						action="/api/upload"
+						encType="multiplart/form-data"
+					>
 						{filesSelected.length > 0 ? (
 							<button type="submit" className={S.Upload__form__btn}>
 								Consultar Nomes
 							</button>
 						) : (
-							<label htmlFor="uploadFile" className={S.Upload__form__btn} tabIndex={0}>
-								Selecionar arquivo EXCEL
+							<>
+								<button
+									className={S.Upload__form__btn}
+									onClick={() => hiddenInputFile.current?.click()}
+									type="button"
+								>
+									Selecionar Arquivo Excel
+								</button>
 								<input
+									ref={hiddenInputFile}
 									className={S.Upload__form__btn__file}
 									type="file"
 									id="uploadFile"
 									multiple
-									tabIndex={1}
-									autoFocus
 									onChange={handleFileChange}
 									accept="
-										.xlsx,
-										.xls,
-										application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
-										application/vnd.ms-excel"
+									.xlsx,
+									.xls,
+									application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+									application/vnd.ms-excel"
 									required
+									aria-hidden="true"
 								/>
-							</label>
+							</>
 						)}
 					</form>
 				</section>
---
+++ src/components/UploadList/index.tsx
@@ -0,0 +1,50 @@
+import { FileItem, FileItems } from '@/types'
+import Icon from '../UI/Icon'
+import { Dispatch, SetStateAction } from 'react'
+import S from './styles.module.scss'
+
+export default function UploadList({
+	files,
+	setFiles,
+}: {
+	files: FileItems
+	setFiles: Dispatch<SetStateAction<FileItems>>
+}) {
+	return (
+		<>
+			{/* <span className={S.Message} role="log" aria-live="assertive" aria-atomic="true">
+				{message ? message : 'teste'}
+			</span> */}
+			<ul className={S.Upload__list} aria-label="Lista arquivos adicionados">
+				{files.map((file: FileItem) => (
+					<li key={file.id} className={S.Upload__list__item}>
+						<Icon ariahidden alt="" name="excel" width="32" height="32" />
+						<span className={S.Upload__list__item__name}>{file.name}</span>
+						<button
+							className={S.Upload__list__item__btn}
+							aria-label="Remover arquivo"
+							onClick={() => handleRemoveFile(file.id, files, setFiles)}
+						>
+							<Icon
+								className={S.Upload__list__item__btn__remove}
+								alt=""
+								name="remove"
+								ariahidden
+							/>
+						</button>
+					</li>
+				))}
+			</ul>
+		</>
+	)
+}
+
+function handleRemoveFile(
+	id: FileItem['id'],
+	files: FileItems,
+	setFiles: Dispatch<SetStateAction<FileItems>>,
+): void {
+	if (!files) return
+
+	setFiles(files.filter((item) => item.id !== id))
+}
