dfb101343745395874ad91827e7e3c19a0910f8b
Eduardo Ferras <edferras@gmail.com>
2025-12-30 17:24:16 -0300

    remove unused code

--- apps/web/src/lib/fetchDomain.ts
+++ apps/web/src/lib/fetchDomain.ts
@@ -1,31 +1,7 @@
 import type FileItem from "@/interfaces/FileItem";
 
-export async function fetchDomain(files: FileItem[]) {
+export async function fetchDomain(_files: FileItem[]) {
   try {
-    const dataForm = new FormData();
-
-    files.forEach((file) => {
-      dataForm.append("files", file.file);
-    });
-
-    // await fetch(urlReq, {
-    // 	method,
-    // 	body: dataForm,
-    // })
-    // 	.then((res) => {
-    // 		if (!res.ok) {
-    // 			throw new Error()
-    // 		}
-
-    // 		return res.blob()
-    // 	})
-    // 	.then((fileBlob) => {
-    // 		const urlFile = URL.createObjectURL(new Blob([fileBlob]))
-    // 			.split('/')
-    // 			.slice(3)
-    // 		return 'file-example-download'
-    // 	})
-
     return "file-example-download";
   } catch (error) {
     console.error(error);
