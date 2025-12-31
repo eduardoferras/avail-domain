099042dae4e9a08700c656f2a45591da513bb126
Eduardo Ferras <edferras@gmail.com>
2025-12-31 15:18:34 -0300

    refactor(web): update h1 in Home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={S.container}>
       <section className={S.Upload}>
         <h1 className={S.Upload__title}>
-          Verificar disponibilidade domínio no Registro.br
+          Verificar disponibilidade de domínios
         </h1>
         <p className={S.Upload__description}>
           Importe uma lista de nomes em Excel para verificar se já possuem
